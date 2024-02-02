d989b34b953ee93bcc92f715710d89193fe8b8a8
Tobias Odermatt <tobias.odermatt@stud.edubs.ch>
2024-02-03 00:22:59 +0100

    Fix UT

--- mongodbweb.Server.Tests/Helpers/DBConnectorTests.cs
+++ mongodbweb.Server.Tests/Helpers/DBConnectorTests.cs
@@ -8,17 +8,10 @@ namespace mongodbweb.Server.Tests.Helpers
     {
         private readonly DbConnector _dbConnector = new ();
 
-        [SetUp]
-        public void Setup()
-        {
-            Environment.SetEnvironmentVariable("MONGODB_CONNECTION_STRING", "mongodb://user:hallo1234@localhost:27017");
-        }
-
         [Test]
         public void DbConnect_Is_MongoDb_Alive()
         {
-            var connectionString = Environment.GetEnvironmentVariable("MONGODB_CONNECTION_STRING") ?? "";
-            var client = DbConnector.DbConnect(connectionString);
+            var client = DbConnector.DbConnect(TestSetup.GetConnectionString());
             Assert.That(client, Is.Not.Null);
 
             var database = client.GetDatabase("UnitTestDb");
